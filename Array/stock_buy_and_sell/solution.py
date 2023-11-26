5e2ecc73288ad70d211ed923869300ef1edf9c97
Sayan-dev <sayanta2702@gmail.com>
2023-11-26 21:04:01 +0530

    Optimized stock

--- Array/stock_buy_and_sell/solution.py
+++ Array/stock_buy_and_sell/solution.py
@@ -1,23 +1,15 @@
+import sys
 class Solution:
 
     def stockBuyAndSell(self, stocks: list[int]):
         # Write your code here
-        sorted_stocks = stocks.copy()
-        sorted_stocks.sort()
-        minim=sorted_stocks[0]
-
-        min_index= 0
-        
+        min_item = sys.maxsize
         max_pro = 0
 
-        for i in range(1,len(stocks)):
-            if stocks[i]==minim: 
-                min_index = i
-                break
+        for i in range(len(stocks)):
+            min_item = min(min_item, stocks[i])
+            max_pro = max(stocks[i] - min_item, max_pro)
 
-        for i in range(min_index, len(stocks)):
-            if stocks[i]-stocks[min_index] > max_pro:
-                max_pro = stocks[i]-stocks[min_index]
 
 
         return max_pro
